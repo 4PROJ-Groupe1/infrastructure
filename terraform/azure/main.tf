fbbe9e99cc7c44702019b979dc3b451380854a28
XamYp <>
2021-02-12 00:01:31 +0100

    Configuration réseau et VPN Azure vérifié

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -24,7 +24,7 @@ resource "azurerm_subnet" "AzureTerraformSubnet" {
   name                 = "GatewaySubnet"
   resource_group_name  = azurerm_resource_group.AzureTerraformGroup.name
   virtual_network_name = azurerm_virtual_network.AzureTerraformNetwork.name
-  address_prefixes     = ["172.32.1.0/24"]
+  address_prefixes     = ["172.32.0.0/24"]
 }
 
 resource "azurerm_public_ip" "AzureTerraformPubIP" {
