c43bb757b2ef0a1f0a60e5cabd8e85412639c2a4
XamYp <>
2021-02-11 01:04:47 +0100

    Configuration réseau et VPN Azure terminée

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -3,20 +3,20 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "AzureTerraformGroup" {
-  name     = "4PROJTEST"
+  name     = "4PROJ"
   location = "North Europe"
 }
 
 resource "azurerm_virtual_network" "AzureTerraformNetwork" {
-  name                = "vNetAzuretest"
+  name                = "vNetAzure"
   resource_group_name = azurerm_resource_group.AzureTerraformGroup.name
   location            = azurerm_resource_group.AzureTerraformGroup.location
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["172.32.0.0/16"]
   depends_on = [azurerm_resource_group.AzureTerraformGroup]
 
     subnet {
-    name           = "ProductionNetworkSubnetTest"
-    address_prefix = "10.0.1.0/24"
+    name           = "ProductionNetworkSubnet"
+    address_prefix = "172.32.1.0/24"
   }
 }
 
@@ -24,11 +24,11 @@ resource "azurerm_subnet" "AzureTerraformSubnet" {
   name                 = "GatewaySubnet"
   resource_group_name  = azurerm_resource_group.AzureTerraformGroup.name
   virtual_network_name = azurerm_virtual_network.AzureTerraformNetwork.name
-  address_prefixes     = ["10.0.0.0/24"]
+  address_prefixes     = ["172.32.1.0/24"]
 }
 
 resource "azurerm_public_ip" "AzureTerraformPubIP" {
-  name                = "4PROJ-VPN-IPtest"
+  name                = "4PROJ-VPN-IP"
   location            = azurerm_resource_group.AzureTerraformGroup.location
   resource_group_name = azurerm_resource_group.AzureTerraformGroup.name
 
@@ -36,7 +36,7 @@ resource "azurerm_public_ip" "AzureTerraformPubIP" {
 }
 
 resource "azurerm_virtual_network_gateway" "AzureTerraformNetworkGateway" {
-  name                = "VPNAzureToOnpremtest"
+  name                = "VPNAzureToOnprem"
   location            = azurerm_resource_group.AzureTerraformGroup.location
   resource_group_name = azurerm_resource_group.AzureTerraformGroup.name
 
@@ -57,15 +57,15 @@ resource "azurerm_virtual_network_gateway" "AzureTerraformNetworkGateway" {
 }
 
 resource "azurerm_local_network_gateway" "AzureTerraformLocalNetworkGateway" {
-  name                = "LocalNetworktest"
+  name                = "LocalNetwork"
   resource_group_name = azurerm_resource_group.AzureTerraformGroup.name
   location            = azurerm_resource_group.AzureTerraformGroup.location
   gateway_address     = "79.80.64.64"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.30.0/24"]
 }
 
 resource "azurerm_virtual_network_gateway_connection" "AzureTerraformNetworkGatewayConnection" {
-  name                = "AzureToFortitest"
+  name                = "AzureToForti"
   location            = azurerm_resource_group.AzureTerraformGroup.location
   resource_group_name = azurerm_resource_group.AzureTerraformGroup.name
 
